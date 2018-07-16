3b184fa3856a040e4001c7a3fa471ae0068bc97b
Teodor Moroz <teomoroz@gmail.com>
2018-07-16 07:57:35 +0300

    Added feature extractor

--- src/spam_classifier.py
+++ src/spam_classifier.py
@@ -1,7 +1,7 @@
 # SpamClassifier
 #Model for checking if the mail is spam or not
 
-import os
+import os, re
 import codecs
 
 from sklearn.pipeline import Pipeline, make_pipeline, FeatureUnion
@@ -12,23 +12,57 @@ import numpy as np
 DATA_DIR = '../data/spam_2'
 
 
-def getFeatures(file_data):
-    #features = []
-    if file_data.find("\n\n"):
-        return 1
+def addSimpleFeature(text_substr, text_body, features):
+    if re.search(text_substr, text_body,  re.IGNORECASE):
+        features.append(1)
     else:
-        return 0
+        features.append(0)
+
+
+def getFeatures(file_data):
+    features = []
+    start_index = file_data.find("\n\n")
+    if start_index == -1:
+        return features
+
+    text_body = file_data[start_index:]
+
+    addSimpleFeature("offer good", text_body, features)
+    addSimpleFeature("receive a bonus", text_body, features)
+    addSimpleFeature("Your Winnings", text_body, features)
+    addSimpleFeature("offer is valid", text_body, features)
+    addSimpleFeature("You Won", text_body, features)
+    addSimpleFeature("cash", text_body, features)
+    addSimpleFeature("GET PAID", text_body, features)
+    addSimpleFeature("STOLEN HOME VIDEO", text_body, features)
+
+    expr = re.compile("TAKE \$[^\]]+ OFF")
+    if expr.match(text_body.upper()):
+        features.append(1)
+    else:
+        features.append(0)
+
+    if text_body.count('!') > 1:
+        features.append(1)
+    else:
+        features.append(0)
+
+    upper_sum = sum(1 for c in text_body if c.isupper())
+    if upper_sum / len(text_body) > 0.5:
+        features.append(1)
+    else:
+        features.append(0)
+
+    return features
+
 
 def scanFiles():
-    num =0;
     for (dirpath, dirnames, filenames) in os.walk(DATA_DIR):
         for file in filenames:
             full_path = os.path.join(dirpath, file)
             with codecs.open(full_path, 'r', encoding='"ISO-8859-1') as filehandle:
                 file_data = filehandle.read()
-                num += getFeatures(file_data)
-    print('Total num - ', num)
-
+                feature_vec = getFeatures(file_data)
 
 
 
