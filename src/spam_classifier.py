158e064ef8f6210159d4402e4e946372df51a2a5
Teodor Moroz <teomoroz@gmail.com>
2018-07-18 08:32:48 +0300

    Build random forest model

--- src/spam_classifier.py
+++ src/spam_classifier.py
@@ -5,11 +5,14 @@ import os, re
 import codecs
 
 from sklearn.pipeline import Pipeline, make_pipeline, FeatureUnion
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
 import pandas as pd
 import numpy as np
 
 
-DATA_DIR = '../data/spam_2'
+SPAM_DATA_DIR = '../data/spam_2'
+NON_SPAM_DATA_DIR = '../data/easy_ham'
 
 
 def addSimpleFeature(text_substr, text_body, features):
@@ -19,14 +22,8 @@ def addSimpleFeature(text_substr, text_body, features):
         features.append(0)
 
 
-def getFeatures(file_data):
+def extractSpamFeatures(text_body):
     features = []
-    start_index = file_data.find("\n\n")
-    if start_index == -1:
-        return features
-
-    text_body = file_data[start_index:]
-
     addSimpleFeature("offer good", text_body, features)
     addSimpleFeature("receive a bonus", text_body, features)
     addSimpleFeature("Your Winnings", text_body, features)
@@ -42,7 +39,7 @@ def getFeatures(file_data):
     else:
         features.append(0)
 
-    if text_body.count('!') > 1:
+    if text_body.count('!') > 2:
         features.append(1)
     else:
         features.append(0)
@@ -56,10 +53,35 @@ def getFeatures(file_data):
     return features
 
 
-def getFeaturesMatrix():
+def extractNotSpamFeatures(text_body):
+    features = []
+
+    addSimpleFeature("Original Message", text_body, features)
+    addSimpleFeature("I don't think", text_body, features)
+    addSimpleFeature("I think", text_body, features)
+    addSimpleFeature("Wrote", text_body, features)
+
+    return features
+
+
+def getFeatures(file_data):
+    features = []
+    start_index = file_data.find("\n\n")
+    if start_index == -1:
+        return features
+
+    text_body = file_data[start_index:]
+    spam_features = extractSpamFeatures(text_body)
+    not_spam_features = extractNotSpamFeatures(text_body)
+
+    return (spam_features + not_spam_features)
+
+
+
+def getFeaturesMatrix(dir):
     features_matrix = []
     count = 0
-    for (dirpath, dirnames, filenames) in os.walk(DATA_DIR):
+    for (dirpath, dirnames, filenames) in os.walk(dir):
         for file in filenames:
             full_path = os.path.join(dirpath, file)
             with codecs.open(full_path, 'r', encoding='"ISO-8859-1') as filehandle:
@@ -83,7 +105,21 @@ def getFeaturesMatrix():
 
 def main():
     print("Starting the classifier...")
-    features_matrix = getFeaturesMatrix()
+    features_matrix_spam = getFeaturesMatrix(SPAM_DATA_DIR)
+    y_spam = np.full((len(features_matrix_spam)), 1)
+
+    features_matrix_data = getFeaturesMatrix(NON_SPAM_DATA_DIR)
+    y_data = np.full((len(features_matrix_data)), 0)
+
+    general_matrix = np.concatenate((features_matrix_data, features_matrix_spam), axis=0)
+    y_results = np.concatenate((y_data, y_spam))
+
+    forest_clf = RandomForestClassifier(random_state=42)
+    forest_clf.fit(general_matrix, y_results)
+    scores = cross_val_score(forest_clf, general_matrix, y_results, scoring="accuracy", cv=10)
+    print(scores)
+
+
 
 
 if __name__ == "__main__":
